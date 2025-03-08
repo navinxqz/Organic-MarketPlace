a7ae0d33b92ecde14e767e29407c25d737650313
Navin <navinmdnawshin@gmail.com>
2025-03-08 19:49:46 +0600

    basic form style added with alignment

--- home.php
+++ home.php
@@ -1,7 +1,50 @@
 <html>
 <head>
-    <title>Home</title> 
+    <title>Organic Food Marketplace</title> 
 </head>
+
 <body>
-    <h1>Welcome to Home Page</h1>
+    <h1 align="center">Seller Registration</h1>
+    <form action="seller.php" method="post">
+        <table align="center" cellpadding="10" cellspacing="0">
+            <tr>
+                <td><strong>Full Name:</strong></td>
+                <td><input type="text" name="fname" required></td>
+            </tr>
+            <tr>
+                <td><strong>Email:</strong></td>
+                <td><input type="email" name="email" required></td>
+            </tr>
+            <tr>
+                <td><strong>Phone:</strong></td>
+                <td><input type="text" name="phone" required></td>
+            </tr>
+            <tr>
+                <td><strong>Address:</strong></td>
+                <td><input type="text" name="address" required></td>
+            </tr>
+            <tr>
+                <td><strong>City:</strong></td>
+                <td><input type="text" name="city" required></td>
+            </tr>
+            <tr>
+                <td><strong>Country:</strong></td>
+                <td><input type="text" name="country" required></td>
+            </tr>
+            <tr>
+                <td><strong>Username:</strong></td>
+                <td><input type="text" name="username" required></td>
+            </tr>
+            <tr>
+                <td><strong>Password:</strong></td>
+                <td><input type="password" name="password" required></td>
+            </tr>
+            <tr>
+                <!-- <td><input type="submit" value="Register"></td> -->
+                 <td colspan="2" align="center">
+                    <input type="submit" value="Register">
+                    <input type="reset" value="Reset">
+                </td>
+            </tr>
+        </table>
 </html>
