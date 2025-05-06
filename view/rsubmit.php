d21372d9ac07fbcdfed518a5e5e9c2a9a9fded67
Maharin Afroj Richi <maharinafrojrichi@gmail.com>
2025-05-06 16:07:31 +0600

    ddfref

--- view/rsubmit.php
+++ view/rsubmit.php
@@ -1,8 +1,4 @@
 
     <h1>SUBMITTED SUCCESSFULLY</h1>
     <p>Thank you for registering as a buyer!</p>
-    <p>Full Name: <?php echo htmlspecialchars($_GET['name']); ?></p>
-    <p>Email: <?php echo htmlspecialchars($_GET['email']); ?></p>
-    <p>Date of Birth: <?php echo htmlspecialchars($_GET['dob']); ?></p>
-    <p>Gender:<?php echo htmlspecialchars($_GET['gender']); ?></p>
-
+    
